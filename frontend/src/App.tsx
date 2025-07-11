de863117030a0cd83fc137ec680f1b22bfe5cf2d
Navu <apple@macbookpro.home>
2025-07-11 15:52:24 -0400

    merged by work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,27 @@
 
 import { Provider } from "react-redux";
 import "./App.css";
+<<<<<<< Updated upstream
 import { Navbar } from "./Components/Navbar/Navbar";
 import PromptComponent from './Components/Prompt'
 import { store } from "./store/store";
 import { DescriptionAlerts } from "./Components/Alert/AlertMessage";
+=======
+>>>>>>> Stashed changes
 
+import PromptComponent from './Components/Prompt'
+import Home from "./Components/home/Home";
 function App() {
   return (
+<<<<<<< Updated upstream
     <Provider store={store}>
       <h1>Prompt Sender</h1>
       <Navbar />
+=======
+    <>
+
+     <Home/>
+>>>>>>> Stashed changes
       <PromptComponent/>
       <DescriptionAlerts />
     </Provider>
