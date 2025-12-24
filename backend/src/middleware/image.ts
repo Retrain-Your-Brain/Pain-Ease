43629c484cee597f5fb7c97e106d89c6f4ca7a4b
navu <navdishabhakri@gmail.com>
2025-12-23 21:00:13 -0500

    adding remaining models and middlewares

---
+++ backend/src/middleware/image.ts
@@ -0,0 +1,17 @@
+mport multer from 'multer'; 
+import path from 'path';
+
+// store in uploads folder with original name
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // make sure 'uploads/' exists
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
---
+++ backend/src/middleware/isAuth.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+  id: string;
+  username: string;
+  iat: number;
+  exp: number;
+}
+
+declare global{
+  namespace Express{
+    interface Request{
+      user?: {
+        id: string;
+        username: string;
+      }
+    }
+  }
+};
+
+
+const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const headerObj = req.headers;
+    if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
+      res.status(401).json({ message: "Not authorized, no token provided" });
+      return;
+    }
+
+    const token = headerObj?.authorization?.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ message: "Not authorized, token missing" });
+      return;
+    }
+
+    const decoded = jwt.verify(token, "navu") as unknown as JwtPayload;
+
+    req.user = { id: decoded.id, username: decoded.username }; // Attach user info to request
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Not authorized, token invalid" });
+  }
+};
+
+export default isAuthenticated;
